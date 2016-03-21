648c5607e2d10559cab42e6106fd68d42f7475fe
Shiyu Dong <shiyud@andrew.cmu.edu>
2016-03-21 17:22:07 -0400

    first draft of Transmission.m

--- DarkChannel/Transmission.m
+++ DarkChannel/Transmission.m
@@ -1,4 +1,14 @@
 %% step 2: transmission - by Shiyu Dong
 function t_ = Transmission(img, A, patch_size, omega)
 
+% Normalize 3 channels of img according to A
+norm_img = double(img)./A;
+
+% dark channel
+dark_channel = CalcDarkChannel(norm_img, patch_size);
+
+% t_
+t_ = 1 - omega*dark_channel;
+
+
 end
