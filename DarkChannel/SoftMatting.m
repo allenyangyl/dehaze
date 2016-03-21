9cf905759a3a9ffd4a5506ebc35e79c011b35167
Allen <allenyangyl@gmail.com>
2016-03-21 16:27:14 -0400

    SoftMatting.m

--- DarkChannel/SoftMatting.m
+++ DarkChannel/SoftMatting.m
@@ -1,7 +1,18 @@
 %% step 3: soft matting - by Yilin Yang
 function t = SoftMatting(img, t_, lambda, epsilon)
 
-L = getLaplacian(img, epsilon, win_size);
+% get Laplacian
 win_size = 1;
+L = getLaplacian(img, epsilon, win_size);
+% image size
+[h, w, ~] = size(img);
+img_size = w * h;
+% get U
+vals = ones(img_size, 1);
+row_inds = 1 : img_size;
+col_inds = 1 : img_size;
+U = sparse(row_inds, col_inds, vals, img_size, img_size);
+% get t
+t = lambda * (L + lambda * U) \ t_;
 
 end
